e37ff5dd5c3bdd958157756cdffff3ba37a298a3
Rich Gulino <rich.gulino@gmail.com>
2026-09-09 09:10:33 -0400

    Tasks loading from database

--- components/Tasks.tsx
+++ components/Tasks.tsx
@@ -1,7 +1,14 @@
-export default function Tasks() {
+export default function Tasks({ tasks }: any) {
 return (
 <div>
-TEST
+Tasks
+Count: {tasks.length}
+
+
+Task List:
+Current Task:
+{tasks[0]?.title}
+New Task
 </div>
 )
 }
